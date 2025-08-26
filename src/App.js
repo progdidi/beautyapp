abe84db60634bbfbf3bcd08171a589b223c958da
Diana Sosnovskaya <progdidi@gmail.com>
2025-08-27 00:35:02 +0300

    feat: feat project structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import {HashRouter as Router, Routes, Route} from "react-router-dom";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Header/>
+
+        <main className="main">
+
+        </main>
+
+        <Footer/>
+      </div>
+    </Router>
+    
   );
 }
 
---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import './header.scss';
+
+const Header = () => {
+    return ( 
+        <header className="header">
+            <div className="container">
+                <h1>dd</h1>
+            </div>
+        </header>
+     );
+}
+ 
+export default Header;
