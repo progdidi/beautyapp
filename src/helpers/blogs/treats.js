2ea6d998d74cbe48b5eaedcc1421f59a0a799d6e
Diana Sosnovskaya <progdidi@gmail.com>
2025-08-29 22:59:34 +0300

    add: add helpers file

---
+++ src/helpers/blogs/treats.js
@@ -0,0 +1,52 @@
+import treat1 from './images/treat1.png';
+import treat2 from './images/treat2.png';
+import treat3 from './images/treat3.png';
+import treat4 from './images/treat4.png';
+import treat5 from './images/treat5.png';
+import treat6 from './images/treat6.png';
+
+const treats = [
+    {
+        id: 1, 
+        title: 'Laser Treatment Consultation',
+        descr: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.',
+        img: treat1
+    },
+
+    {
+        id: 2, 
+        title: 'Cool Sculpting Consultation',
+        descr: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.',
+        img: treat2
+    },
+
+    {
+        id: 3, 
+        title: 'IPL Upper Chest',
+        descr: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.',
+        img: treat3
+    },
+
+    {
+        id: 4, 
+        title: 'Underarm Botox',
+        descr: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.',
+        img: treat4
+    },
+
+    {
+        id: 5, 
+        title: 'Body Botox',
+        descr: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.',
+        img: treat5
+    },
+
+    {
+        id: 6, 
+        title: 'Injectable Filler Consultation',
+        descr: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.',
+        img: treat6
+    },
+
+
+]
